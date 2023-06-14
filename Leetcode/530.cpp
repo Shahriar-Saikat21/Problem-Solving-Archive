15157eec1f4274c16a536f3027f4ea0b5e5bb237
saikat <saikatbass@gmail.com>
2023-06-14 23:39:33 +0600

    14 June 2023

---
+++ Leetcode/530.cpp
@@ -0,0 +1,33 @@
+
+//Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int prev = INT_MAX;
+    int ans = INT_MAX;
+
+    void inOrder(TreeNode* root){
+        if(root->left!=NULL){
+            inOrder(root->left);
+        }
+        ans = min(ans,abs(root->val-prev));
+        prev = root->val;
+        if(root->right!=NULL){
+            inOrder(root->right);
+        }
+
+    }
+
+    int getMinimumDifference(TreeNode* root) {
+        inOrder(root);
+        return ans;
+    }
+};
